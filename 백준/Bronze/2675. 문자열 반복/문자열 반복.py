5fabd545a518847be060185501b6ff41fdf8f964
이정규 <jeongkyu512@gmail.com>
2024-01-13 10:10:31 +0900

    [Bronze II] Title: 문자열 반복, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.py
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.py
@@ -1,7 +1,7 @@
 Case = int(input())
 
 for i in range(Case):
-    N, S = input().split()
+    N,S = input().split()
     for j in range(len(S)):
         print(S[j] * int(N), end = '')
     print('')
