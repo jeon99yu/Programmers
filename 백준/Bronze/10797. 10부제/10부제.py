76d7033ce9d4324b0f62c40cce308d847cd8ef80
이정규 <jeongkyu512@gmail.com>
2024-01-15 14:52:19 +0900

    [Bronze IV] Title: 10부제, Time: 40 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Bronze/10797. 10부제/10부제.py
+++ 백준/Bronze/10797. 10부제/10부제.py
@@ -1,10 +1,8 @@
-date_digit = int(input())
-car_digits = list(map(int, input().split()))
+d = int(input())
+cars = list(map(int,input().split()))
+count = 0
 
-violating_cars = 0
-
-for car_digit in car_digits:
-    if (date_digit == car_digit) or (date_digit == 0 and car_digit == 10):
-        violating_cars += 1
-
-print(violating_cars)
+for car in cars:
+    if d == car:
+        count+=1
+print(count)
